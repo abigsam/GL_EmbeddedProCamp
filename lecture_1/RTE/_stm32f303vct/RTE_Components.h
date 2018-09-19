b56c827d874ff272cf3dfd555c4e2567c2811f36
abigsam <abigsam@gmail.com>
2018-09-19 23:16:31 +0300

    Add lecture 1

---
+++ lecture_1/RTE/_stm32f303vct/RTE_Components.h
@@ -0,0 +1,22 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'stm32f3_blink' 
+ * Target:  'stm32f303vct' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f3xx.h"
+
+#define RTE_DEVICE_FRAMEWORK_CUBE_MX
+#define RTE_DEVICE_STARTUP_STM32F34XX    /* Device Startup for STM32F3 */
+
+#endif /* RTE_COMPONENTS_H */
