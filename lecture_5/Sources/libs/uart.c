c6f8bc7a93189ddd270e85a2940d730d6ce8f833
abigsam <abigsam@gmail.com>
2018-10-07 21:30:23 +0300

    Add UART functiones prototypes

--- lecture_5/Sources/libs/uart.c
+++ lecture_5/Sources/libs/uart.c
@@ -1,6 +1,60 @@
-
+/*
+		STM32F3 driver for UART, source
+*/
 
 
 #include "uart.h"
 
+/**
+  * @brief  Init UART with specified parameters.
+	*
+  * @param  uartx: pointer to UARTx
+	* @param  baud_rate: Uart baud rate
+	* @param  wlen: UART word length, can be one of:
+	*					UART_Word_8bit, UART_Word_9bit
+	* @param  parity: configure parity bit, can be one of:
+	*					UART_Parity_None, UART_Parity_Even, UART_Parity_Odd
+	* @param  sbit: configure UART stop bit length, can be one of:
+	*					UART_Stopbit_1, UART_Stopbit_2, UART_Stopbit_0_5, UART_Stopbit_1_5
+  * @retval None
+  */
+void uart_init(USART_TypeDef *uartx, uint16_t baud_rate, UART_WordLen wlen, UART_Parity parity, UART_Stopbits sbit)
+{
+	
+}
 
+
+/**
+  * @brief  Open UART to start comunication
+	*
+  * @param  uartx: pointer to UARTx
+  * @retval None
+  */
+void uart_open(USART_TypeDef *uart)
+{
+	
+}
+
+
+/**
+  * @brief  Close UART to start comunication
+	*
+  * @param  uartx: pointer to UARTx
+  * @retval None
+  */
+void uart_close(USART_TypeDef *uart)
+{
+	
+}
+
+
+/**
+  * @brief  Deinit UART hardware to default values
+	*
+  * @param  uartx: pointer to UARTx
+  * @retval None
+  */
+void uart_deinit(USART_TypeDef *uart)
+{
+	
+}
--- lecture_5/Sources/libs/uart.h
+++ lecture_5/Sources/libs/uart.h
@@ -1,4 +1,6 @@
-
+/*
+		STM32F3 driver for UART, header
+*/
 
 #ifndef ___UART_H
 #define ___UART_H
@@ -6,7 +8,28 @@
 
 #include "stm32f303xc.h"
 
+typedef enum {
+	UART_Word_8bit = 0,
+	UART_Word_9bit
+} UART_WordLen;
 
+typedef enum {
+	UART_Parity_None = 0,
+	UART_Parity_Even,
+	UART_Parity_Odd
+} UART_Parity;
+
+typedef enum {
+	UART_Stopbit_1 = 0,
+	UART_Stopbit_2,
+	UART_Stopbit_0_5,
+	UART_Stopbit_1_5
+} UART_Stopbits;
+
+void uart_init(USART_TypeDef *uartx, uint16_t baud_rate, UART_WordLen wlen, UART_Parity parity, UART_Stopbits sbit);
+void uart_open(USART_TypeDef *uart);
+void uart_close(USART_TypeDef *uart);
+void uart_deinit(USART_TypeDef *uart);
 
 
 
